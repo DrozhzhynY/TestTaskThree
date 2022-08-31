b068f9d9448aad064dd77dc19a2a51aa6859179a
DrozhzhynY <drozhzhyn.yevhenii@gmail.com>
2022-08-31 16:57:37 +0300

    Initial Commit

---
+++ TestTaskThree/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestTaskThree
+//
+//  Created by admin on 31.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestTaskThree/TestTaskThreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskThreeApp.swift
+//  TestTaskThree
+//
+//  Created by admin on 31.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskThreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
