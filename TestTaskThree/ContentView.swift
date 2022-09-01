9d82c0673856b21d5ef3f817366f6f0ccf50e19a
DrozhzhynY <drozhzhyn.yevhenii@gmail.com>
2022-09-01 13:54:35 +0300

    initial	 commit  Add one page application

--- TestTaskThree/ContentView.swift
+++ TestTaskThree/ContentView.swift
@@ -1,16 +1,63 @@
 //
 //  ContentView.swift
 //  TestTaskThree
-//
-//  Created by admin on 31.08.2022.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var movies = [String]()
+    @State private var movieTitle: String = ""
+    @State private var movieYear: Int?
+    @State private var duplicate = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TextField("Title", text: $movieTitle)
+                .padding([.top, .leading, .trailing])
+                .textFieldStyle(.roundedBorder)
+            
+            TextField("Year", value: $movieYear, format: .number)
+                .keyboardType(.numberPad)
+                .padding(.horizontal)
+                .textFieldStyle(.roundedBorder)
+            
+            if duplicate {
+                Text("There cannot be duplicates in the movie list")
+                    .font(.footnote)
+                    .foregroundColor(Color.red)
+            }
+            
+            Button("Add") {
+                addNewMovie()
+            }
+                .font(.title2)
+                .foregroundColor(.white)
+                .frame(width: 100, height: 50)
+                .background(Color.blue)
+                .cornerRadius(10)
+            
+            List {
+                Section {
+                    ForEach(movies, id: \.self) {movie in
+                        Text(movie)
+                    }
+                }
+            }
+        }
+        .onSubmit {
+            addNewMovie()
+        }
+    }
+    
+    func addNewMovie() {
+        guard movieTitle.count > 0 && movieYear != nil else { return }
+        let movie = "\(movieTitle) \(movieYear!)"
+        if movies.contains(movie) {
+            duplicate = true
+        } else {
+            movies.insert(movie, at: 0)
+            duplicate = false
+        }
     }
 }
 
